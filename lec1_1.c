e1908c71049bca2b8241aa84d39f83b132b83190
YomnaRagab <52376427+YomnaRagab@users.noreply.github.com>
2019-06-30 15:33:31 +0200

    Add files via upload

---
+++ lec1_1.c
@@ -0,0 +1,21 @@
+/*Author:Yomna Ragab*/
+/* Sum the even number from 1 to n */
+#include <stdio.h>
+void main (void)
+{
+  int sum=0;
+  int x,i;
+  printf("Enter the number of elements:\n");
+  scanf("%d",&x);
+  for(i=0; i<=j; i++)
+  {
+    if(i%2==0)
+    {
+      sum=sum+i;
+    }
+  }
+  printf("The sum is %d",sum);
+//oder
+//for(i=0;i<=x;i+2)
+
+}
